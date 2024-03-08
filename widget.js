dcddc071a1894da0f2fcc7c08205f5879e022279
Stux <100985218+StuxVT@users.noreply.github.com>
2024-03-07 22:39:13 -0500

    init

---
+++ widget.js
@@ -0,0 +1,29 @@
+window.addEventListener('onWidgetLoad', function (obj) {
+  const fieldData = obj.detail.fieldData;
+  let countdownTime = fieldData.duration;
+  let timerElement = document.getElementById("timer");
+  let initialMessage = fieldData.initialMessage
+  let expiredMessage = fieldData.expiredMessage
+
+  // set bg color
+  timerElement.style.backgroundColor = fieldData.backgroundColor
+
+  // seconds -> min:sec format
+  function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return `${minutes}:${seconds}`;
+  }
+
+  //run every 1s
+  let countdownInterval = setInterval(function() {
+    if (countdownTime >= 0) {
+      timerElement.textContent = initialMessage + " " + formatTime(countdownTime);
+      countdownTime--;
+    } else {
+      clearInterval(countdownInterval);
+      timerElement.textContent = expiredMessage;
+    }
+  }, 1000);
+});
---
+++ widget.json.js
@@ -0,0 +1,22 @@
+let fieldData={
+    "duration": {
+        "label": "Duration in seconds",
+        "type": "number",
+        "value": 120
+    },
+    "initialMessage": {
+        "label": "Initial message",
+        "type": "text",
+        "value": "Countdown:"
+    },
+    "expiredMessage": {
+        "label": "Expired message",
+        "type": "text",
+        "value": "Time's up!"
+    },
+    "backgroundColor": {
+        "label": "Background Color",
+        "type": "colorpicker",
+        "value": "rgba(0, 0, 0, 0.5)"
+    }
+}
