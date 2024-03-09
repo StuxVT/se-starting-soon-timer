6599a4a41c3c4f150cbf89e9d708dda2cfc77e58
Stux <100985218+StuxVT@users.noreply.github.com>
2024-03-08 23:18:01 -0500

    add group to configs

--- widget.json.js
+++ widget.json.js
@@ -2,27 +2,26 @@ let fieldData={
     "duration": {
         "label": "Duration in seconds",
         "type": "number",
-        "value": 120
+        "value": 120,
+        "group": "Behavior"
     },
     "initialMessage": {
         "label": "Initial message",
         "type": "text",
-        "value": "Countdown:"
+        "value": "Countdown:",
+        "group": "Behavior"
     },
     "expiredMessage": {
         "label": "Expired message",
         "type": "text",
-        "value": "Time's up!"
+        "value": "Time's up!",
+        "group": "Behavior"
     },
     "backgroundColor": {
         "label": "Background Color",
         "type": "colorpicker",
-        "value": "rgba(0, 0, 0, 0.5)"
-    },
-    "fontName": {
-        "type": "googleFont",
-        "label": "Google Font name",
-        "value": "Times New Roman"
+        "value": "rgba(0, 0, 0, 0.5)",
+        "group": "Visuals"
     },
     "borderRadius": {
         "type": "slider",
@@ -30,11 +29,19 @@ let fieldData={
         "value": 10,
         "min": 0,
         "max": 100,
-        "step": 1
+        "step": 1,
+        "group": "Visuals"
     },
     "textShadow": {
         "type": "checkbox",
         "label": "Text Shadow",
-        "value": "true"
+        "value": "true",
+        "group": "Visuals"
+    },
+    "fontName": {
+        "type": "googleFont",
+        "label": "Google Font name",
+        "value": "Times New Roman",
+        "group": "Visuals"
     }
 }
