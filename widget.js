dca05e5afe3ac7a70d03aa1834812211cd0d1796
Stux <100985218+StuxVT@users.noreply.github.com>
2024-06-20 21:39:21 -0400

    add commands for changing and resetting the timer

--- widget.js
+++ widget.js
@@ -1,33 +1,51 @@
-window.addEventListener('onWidgetLoad', function (obj) {
+window.addEventListener("onWidgetLoad", function (obj) {
   const fieldData = obj.detail.fieldData;
   let countdownTime = fieldData.duration;
   let timerElement = document.getElementById("timer");
-  let initialMessage = fieldData.initialMessage
-  let expiredMessage = fieldData.expiredMessage
+  let initialMessage = fieldData.initialMessage;
+  let expiredMessage = fieldData.expiredMessage;
 
   // visuals
-  timerElement.style.backgroundColor = fieldData.backgroundColor
-  timerElement.style.fontFamily = fieldData.fontName
-  timerElement.style.color = fieldData.textColor
-  timerElement.style.borderRadius = fieldData.borderRadius + "px"
-  timerElement.style.textShadow = fieldData.textShadow ? "0 0 6px black" : "none"
+  timerElement.style.backgroundColor = fieldData.backgroundColor;
+  timerElement.style.fontFamily = fieldData.fontName;
+  timerElement.style.color = fieldData.textColor;
+  timerElement.style.borderRadius = fieldData.borderRadius + "px";
+  timerElement.style.textShadow = fieldData.textShadow
+    ? "0 0 6px black"
+    : "none";
 
   // seconds -> min:sec format
   function formatTime(time) {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
     return `${minutes}:${seconds}`;
   }
 
   //run every 1s
-  let countdownInterval = setInterval(function() {
+  let countdownInterval = setInterval(function () {
     if (countdownTime >= 0) {
-      timerElement.textContent = initialMessage + " " + formatTime(countdownTime);
+      timerElement.textContent =
+        initialMessage + " " + formatTime(countdownTime);
       countdownTime--;
     } else {
       clearInterval(countdownInterval);
       timerElement.textContent = expiredMessage;
     }
   }, 1000);
+
+  window.addEventListener("onEventReceived", function (obj) {
+    const message = obj.detail.event.data.text;
+    const isBroadcaster =
+      obj.detail.event.data.tags.badges &&
+      obj.detail.event.data.tags.badges.includes("broadcaster/1");
+
+    if (isBroadcaster) {
+      if (message.startsWith("!resetcountdown")) {
+        countdownTime = fieldData.duration;
+      } else if (message.startsWith("!setcountdown ")) {
+        countdownTime = parseInt(message.split(" ")[1])
+      }
+    }
+  });
 });
