cb51802a1766ff54b7124c8959e585b4955d8f11
Stux <100985218+StuxVT@users.noreply.github.com>
2024-07-23 17:37:50 -0400

    fix commands when countdown ended

--- widget.js
+++ widget.js
@@ -4,17 +4,28 @@ window.addEventListener("onWidgetLoad", function (obj) {
   let timerElement = document.getElementById("timer");
   let initialMessage = fieldData.initialMessage;
   let expiredMessage = fieldData.expiredMessage;
+  let countdownInterval;
 
-  // visuals
+  function startCountdown() {
+    clearInterval(countdownInterval);
+    countdownInterval = setInterval(function () {
+      if (countdownTime >= 0) {
+        timerElement.textContent = initialMessage + " " + formatTime(countdownTime);
+        countdownTime--;
+      } else {
+        clearInterval(countdownInterval);
+        timerElement.textContent = expiredMessage;
+      }
+    }, 1000);
+  }
+
+  // Visuals
   timerElement.style.backgroundColor = fieldData.backgroundColor;
   timerElement.style.fontFamily = fieldData.fontName;
   timerElement.style.color = fieldData.textColor;
   timerElement.style.borderRadius = fieldData.borderRadius + "px";
-  timerElement.style.textShadow = fieldData.textShadow
-    ? "0 0 6px black"
-    : "none";
+  timerElement.style.textShadow = fieldData.textShadow ? "0 0 6px black" : "none";
 
-  // seconds -> min:sec format
   function formatTime(time) {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
@@ -22,29 +33,19 @@ window.addEventListener("onWidgetLoad", function (obj) {
     return `${minutes}:${seconds}`;
   }
 
-  //run every 1s
-  let countdownInterval = setInterval(function () {
-    if (countdownTime >= 0) {
-      timerElement.textContent =
-        initialMessage + " " + formatTime(countdownTime);
-      countdownTime--;
-    } else {
-      clearInterval(countdownInterval);
-      timerElement.textContent = expiredMessage;
-    }
-  }, 1000);
+  startCountdown();
 
   window.addEventListener("onEventReceived", function (obj) {
     const message = obj.detail.event.data.text;
-    const isBroadcaster =
-      obj.detail.event.data.tags.badges &&
-      obj.detail.event.data.tags.badges.includes("broadcaster/1");
+    const isBroadcaster = obj.detail.event.data.tags.badges && obj.detail.event.data.tags.badges.includes("broadcaster/1");
 
     if (isBroadcaster) {
       if (message.startsWith("!resetcountdown")) {
         countdownTime = fieldData.duration;
+        startCountdown(); // Restart countdown
       } else if (message.startsWith("!setcountdown ")) {
-        countdownTime = parseInt(message.split(" ")[1])
+        countdownTime = parseInt(message.split(" ")[1]);
+        startCountdown(); // Restart countdown
       }
     }
   });
